32eb02f10e568bfd2242fd50eceab30d650122b3
Jaeson Wang <jaesonwangca@gmail.com>
2024-06-15 15:58:29 -0400

    fixed spacing issue in model.py

--- model.py
+++ model.py
@@ -27,25 +27,21 @@ class PositionalEncoding(nn.Module):
         self.d_model = d_model
         self.seq_len = seq_len
         self.dropout = nn.Dropout(dropout)
-        # Create a matrix of shape (seq_len, d_model)
+        
         pe = torch.zeros(seq_len, d_model)
-        # Create a vector of shape (seq_len)
         position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) # (seq_len, 1)
-        # Create a vector of shape (d_model)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # (d_model / 2)
-        # Apply sine to even indices
+        
         pe[:, 0::2] = torch.sin(position * div_term) # sin(position * (10000 ** (2i / d_model))
-        # Apply cosine to odd indices
         pe[:, 1::2] = torch.cos(position * div_term) # cos(position * (10000 ** (2i / d_model))
-        # Add a batch dimension to the positional encoding
+        
         pe = pe.unsqueeze(0) # (1, seq_len, d_model)
-        # Register the positional encoding as a buffer
-        self.register_buffer('pe', pe)
+        self.register_buffer('pe', pe)# Register the positional encoding as a buffer
 
     def forward(self, x):
-        #x should have size(batch_size, seq_len, d_model) = (32, 10, 512)
-        #pe should have size(1, seq_len, d_model) = (1, 10, 512)
-        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) # (batch, seq_len, d_model)
+        #x should have size(batch_size, seq_len, d_model)
+        #pe should have size(1, seq_len, d_model) 
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
         return self.dropout(x)
 
 
@@ -96,7 +92,6 @@ class MultiHeadAttentionBlock(nn.Module):
         attention_score = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
         if mask is not None:
            attention_score = attention_score.masked_fill_(mask == 0, -1e9)
-           #attention_score.masked_fill_(mask == 0, -1e9) in video above from chatgpt
         attention_score = attention_score.softmax(dim = -1)
         if dropout is not None:
             attention_scores = dropout(attention_score)
