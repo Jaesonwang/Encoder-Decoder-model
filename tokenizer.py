ae2f3100fe0b4283f29eb5a65bf8d65436f1bbd4
Jaeson Wang <jaesonwangca@gmail.com>
2024-07-10 20:36:49 -0400

    Added SOS and EOS tokens to tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -10,6 +10,8 @@ import torch.nn as nn
 # Hexadecimal characters 
 hex_chars = '0123456789ABCDEF'
 hex_vocab = sorted(set(hex_chars))
+hex_vocab.append('<SOS>')
+hex_vocab.append('<EOS>')
 hex_vocab.append('<PAD>')
 hex_char_to_index = {char: idx for idx, char in enumerate(hex_vocab)}
 hex_index_to_char = {idx: char for idx, char in enumerate(hex_vocab)}
@@ -18,6 +20,8 @@ hex_index_to_char = {idx: char for idx, char in enumerate(hex_vocab)}
 # Decimal characters
 dec_chars = '0123456789'
 dec_vocab = sorted(set(dec_chars))
+dec_vocab.append('<SOS>')
+dec_vocab.append('<EOS>')
 dec_vocab.append('<PAD>')
 dec_char_to_index = {char: idx for idx, char in enumerate(dec_vocab)}
 dec_index_to_char = {idx: char for idx, char in enumerate(dec_vocab)}
@@ -29,13 +33,28 @@ def hex_encode(input_string):
 def dec_encode(input_string):
     return [dec_char_to_index[char] for char in input_string]
 
-def hex_pad_sequence(sequence, max_length, padding_value='<PAD>'):
-    padding_length = max_length - len(sequence)
-    return sequence + [hex_char_to_index[padding_value]] * padding_length
+def hex_pad_sequence(sequence, max_length, padding_value='<PAD>', SOS_value = '<SOS>', EOS_value = '<EOS>'):
+    padding_length = max_length - len(sequence) - 2
+    return [hex_char_to_index[SOS_value]] + sequence + [hex_char_to_index[EOS_value]] + [hex_char_to_index[padding_value]] * padding_length
 
-def dec_pad_sequence(sequence, max_length, padding_value='<PAD>'):
-    padding_length = max_length - len(sequence)
-    return sequence + [dec_char_to_index[padding_value]] * padding_length
+def dec_pad_sequence(sequence, max_length, padding_value='<PAD>', SOS_value = '<SOS>', EOS_value = '<EOS>'):
+    padding_length = max_length - len(sequence) -  2
+    return [dec_char_to_index[SOS_value]] + sequence + [dec_char_to_index[EOS_value]] + [dec_char_to_index[padding_value]] * padding_length
 
+encoded_hex = hex_encode("ABCDE")
+padded_hex = hex_pad_sequence(encoded_hex, 15)
+print(padded_hex)
+
+encoded_hex = hex_encode("29433")
+padded_hex = hex_pad_sequence(encoded_hex, 15)
+print(padded_hex)
+
+encoded_hex = hex_encode("A5")
+padded_hex = hex_pad_sequence(encoded_hex, 15)
+print(padded_hex)
+
+encoded_hex = hex_encode("A56743434")
+padded_hex = hex_pad_sequence(encoded_hex, 15)
+print(padded_hex)
 
 
