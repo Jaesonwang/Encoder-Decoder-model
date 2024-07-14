Fixed masking error in training loop

Train.py
@@ -70,7 +70,7 @@ def train():
     tgt_padding_idx = dec_char_to_index['<PAD>']
     
     model = Transformer(seq_length, input_dim, output_dim, d_model, num_heads, num_encoder_layers, num_decoder_layers, dropout)
-    criterion = nn.CrossEntropyLoss(ignore_index=src_padding_idx, label_smoothing=0.1)
+    criterion = nn.CrossEntropyLoss(ignore_index=tgt_padding_idx, label_smoothing=0.1)
     criterion2 = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     

sample.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 from model import Transformer
-from Train import seq_length, d_model, num_heads, num_layers, dropout
+from Train import seq_length, d_model, num_heads, num_encoder_layers, num_decoder_layers, dropout
 from tokenizer import hex_encode, hex_pad_sequence, dec_index_to_char, dec_char_to_index
 
-model = Transformer(seq_length, 19, 13, d_model, num_heads, num_layers, dropout)
+model = Transformer(seq_length, 19, 13, d_model, num_heads, num_encoder_layers, num_decoder_layers, dropout)
 model.load_state_dict(torch.load('transformer_model.pth'))
 model.eval()
 