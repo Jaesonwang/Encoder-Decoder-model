78a5a52954c3bf99d9a52e53979bf0a268b21dfa
Jaeson Wang <jaesonwangca@gmail.com>
2024-07-14 21:07:00 -0400

    minor changes to sample.py

--- sample.py
+++ sample.py
@@ -5,29 +5,24 @@ from model import Transformer
 from Train import seq_length, d_model, num_heads, num_encoder_layers, num_decoder_layers, dropout, max_length
 from tokenizer import hex_encode, hex_pad_sequence, hex_char_to_index, dec_char_to_index, dec_index_to_char
 
-# Load model
 model = Transformer(seq_length, 19, 13, d_model, num_heads, num_encoder_layers, num_decoder_layers, dropout)
 model.load_state_dict(torch.load('transformer_model.pth'))
 model.eval()
 
-# Function to generate random hexadecimal string
 def generate_random_hex(length):
     rand_int = secrets.randbits(length * 4)       # Multiply by 4 to convert bytes to hex digits
     hex_string = format(rand_int, f'0{length}x')  # Convert the integer to a hexadecimal string
     hex_string = hex_string.upper()
     return hex_string
 
-# Generate random hex string and prepare inputs
 random_hex = generate_random_hex(6)
 hex_encoded = hex_encode(random_hex)
 hex_padded = hex_pad_sequence(hex_encoded, max_length)
 input_tensor = torch.tensor([hex_padded])
 
-# Padding indices
 src_padding_idx = hex_char_to_index['<PAD>']
 tgt_padding_idx = dec_char_to_index['<PAD>']
 
-# Function to create masks
 def create_mask(src, tgt, src_padding_idx, tgt_padding_idx):
     src_mask = (src != src_padding_idx).unsqueeze(-2)
     tgt_mask = (tgt != tgt_padding_idx).unsqueeze(-2)
@@ -36,10 +31,8 @@ def create_mask(src, tgt, src_padding_idx, tgt_padding_idx):
     tgt_mask = tgt_mask & (nopeak_mask == 0)
     return src_mask, tgt_mask
 
-# Dummy targets for inference
 dummy_targets = torch.zeros((1, seq_length), dtype=torch.long)
 
-# Inference
 with torch.no_grad():
     src_mask, tgt_mask = create_mask(input_tensor, dummy_targets, src_padding_idx, tgt_padding_idx)
     print("src_mask:", src_mask)
@@ -47,11 +40,9 @@ with torch.no_grad():
     outputs = model(input_tensor, dummy_targets, src_mask, tgt_mask)
     print("outputs:", outputs)
 
-# Post-processing predictions
 _, predicted_indices = torch.max(outputs, dim=-1)
 print("predicted_indices:", predicted_indices)
 
-# Further decoding and printing results
 predicted_decoded = [dec_index_to_char[idx.item()] for idx in predicted_indices.squeeze()]
 translated_output = ''.join(predicted_decoded).strip('<PAD>')  # Remove padding tokens
 
