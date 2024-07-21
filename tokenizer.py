473fcc72ce4452ee5a70871be81156174f70c833
Jaeson Wang <jaesonwangca@gmail.com>
2024-07-21 18:03:02 -0400

    added decode function in tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -13,6 +13,7 @@ hex_vocab = sorted(set(hex_chars))
 hex_vocab.append('<SOS>')
 hex_vocab.append('<EOS>')
 hex_vocab.append('<PAD>')
+hex_vocab.append('<UNK>')
 hex_char_to_index = {char: idx for idx, char in enumerate(hex_vocab)}
 hex_index_to_char = {idx: char for idx, char in enumerate(hex_vocab)}
 
@@ -23,6 +24,7 @@ dec_vocab = sorted(set(dec_chars))
 dec_vocab.append('<SOS>')
 dec_vocab.append('<EOS>')
 dec_vocab.append('<PAD>')
+dec_vocab.append('<UNK>')
 dec_char_to_index = {char: idx for idx, char in enumerate(dec_vocab)}
 dec_index_to_char = {idx: char for idx, char in enumerate(dec_vocab)}
 
@@ -37,6 +39,27 @@ def hex_pad_sequence(sequence, max_length, padding_value='<PAD>', SOS_value = '<
     padding_length = max_length - len(sequence) - 2
     return [hex_char_to_index[SOS_value]] + sequence + [hex_char_to_index[EOS_value]] + [hex_char_to_index[padding_value]] * padding_length
 
-def dec_pad_sequence(sequence, max_length, padding_value='<PAD>', SOS_value = '<SOS>', EOS_value = '<EOS>'):
-    padding_length = max_length - len(sequence) -  2
-    return [dec_char_to_index[SOS_value]] + sequence + [dec_char_to_index[EOS_value]] + [dec_char_to_index[padding_value]] * padding_length
+def dec_pad_sequence(sequence, max_length, padding_value='<PAD>', SOS_value = '<SOS>'):
+    padding_length = max_length - len(sequence) -  1
+    return [dec_char_to_index[SOS_value]] + sequence  + [dec_char_to_index[padding_value]] * padding_length
+
+def label_pad_sequence(sequence, max_length, padding_value='<PAD>', EOS_value = '<EOS>'):
+    padding_length = max_length - len(sequence) -  1
+    return sequence  + [dec_char_to_index[EOS_value]] + [dec_char_to_index[padding_value]] * padding_length
+
+def dec_decode(indices):
+    """
+    Decodes a tensor or NumPy array of indices into a string.
+    """
+    dec_index_to_char = {v: k for k, v in dec_char_to_index.items()}  # Assuming dec_char_to_index is the reverse mapping
+    
+    if isinstance(indices, torch.Tensor):
+        indices = indices.cpu().numpy()  # Convert tensor to numpy array if needed
+    
+    decoded_chars = [dec_index_to_char.get(idx, '<UNK>') for idx in indices.flatten()]
+    
+    # Handle cases where indices are in a 2D array or more dimensions
+    decoded_string = ''.join(decoded_chars)
+    
+    return decoded_string
+
